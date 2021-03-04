55053cd6dea99f6063b76d5cc3d496538ba3a756
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2021-03-04 20:05:00 +0500

    vc and vp

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ app.listen(port, () => {
     console.log(`server is live on port ${port}`)
 });
 
-
-const uri = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE2MTM1MDk1NTcsInR5cGUiOiJzZHIiLCJ0YWciOiJTRFIiLCJyZXBseVRvIjpbInNhYmhpLWRldmVsb3BtZW50LXNlcnZlciJdLCJyZXBseVVybCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTIzNDUvdXNlci92cCIsImNsYWltcyI6W3sicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJmdWxsbmFtZSJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJhZGRyZXNzIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6InBob25lbnVtYmVyIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6ImVtYWlsIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6ImRvYiJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJwaG90byJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJuYXRpeyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE2MTM1MDk1NTcsInR5cGUiOiJzZHIiLCJ0YWciOiJTRFIiLCJyZXBseVRvIjpbInNhYmhpLWRldmVsb3BtZW50LXNlcnZlciJdLCJyZXBseVVybCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTIzNDUvdXNlci92cCIsImNsYWltcyI6W3sicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJmdWxsbmFtZSJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJhZGRyZXNzIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6InBob25lbnVtYmVyIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6ImVtYWlsIn0seyJyZWFzb24iOiJXZSB3aWxsIHNob3cgdGhpcyBvbiB5b3VyIHByb2ZpbGUiLCJlc3NlbnRpYWwiOnRydWUsImNsYWltVHlwZSI6ImRvYiJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJwaG90byJ9LHsicmVhc29uIjoiV2Ugd2lsbCBzaG93IHRoaXMgb24geW91ciBwcm9maWxlIiwiZXNzZW50aWFsIjp0cnVlLCJjbGFpbVR5cGUiOiJuYXRpb25hbElkIn1dLCJjcmVkZW50aWFscyI6WyJKV1QtcHVibGljLXByb2ZpbGUuLi4iXSwiaXNzIjoiZGlkOmV0aHI6cmlua2VieToweGI0ZTJlMmE5M2U0NzA1MzI0MjNiYjZlMzQ3ODJjNjc3ZTljYjc4Y2UifQ.aJ36tSr3Hh6wRVhnXPFIg4WOK-Wev4_hqOpchwfAlgJBAeQe-K5gb0TW8kv0cH9qUc68D970iYWyT8MDfNG8QwAb25hbElkIn1dLCJjcmVkZW50aWFscyI6WyJKV1QtcHVibGljLXByb2ZpbGUuLi4iXSwiaXNzIjoiZGlkOmV0aHI6cmlua2VieToweGI0ZTJlMmE5M2U0NzA1MzI0MjNiYjZlMzQ3ODJjNjc3ZTljYjc4Y2UifQ.aJ36tSr3Hh6wRVhnXPFIg4WOK-Wev4_hqOpchwfAlgJBAeQe-K5gb0TW8kv0cH9qUc68D970iYWyT8MDfNG8QwA";
-// console.log(uri.length);
 // creating homepage
 app.get('/', (req, res) => {
     res.send({
@@ -103,7 +100,7 @@ app.post('/vp', async (req, res) => {
             proofFormat: 'jwt',
         });
 
-
+        // console.log(verifiableCredential);
         const verifiablePresentation = await agent.createVerifiablePresentation({
             presentation: {
                 verifier: [uri],
@@ -117,6 +114,7 @@ app.post('/vp', async (req, res) => {
             save: true,
         });
         const response = await apiInstance.post('user/vp', verifiablePresentation);
+        // console.log('###############################################################################');
         console.log(response);
         res.send(verifiablePresentation);
     } catch (error) {
